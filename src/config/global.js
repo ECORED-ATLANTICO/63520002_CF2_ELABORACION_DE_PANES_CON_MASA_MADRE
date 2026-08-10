d556d7804cbe54d08c07651884f8a9a0527245b9
Carlos Andres Diaz Pinto <98177573+AndresDiazPinto@users.noreply.github.com>
2026-08-09 20:51:22 -0500

    CFA

--- src/config/global.js
+++ src/config/global.js
@@ -205,7 +205,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/63520002_CF02_DU.pdf',
       },
       {
         icono: 'fas fa-download',
