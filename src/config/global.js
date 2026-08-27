e7be62b184157aa61dec5438337e407e1048dc4a
Carlos Andres Diaz Pinto <98177573+AndresDiazPinto@users.noreply.github.com>
2026-08-26 22:15:56 -0500

    Empaquetado ajustes RED final

--- src/config/global.js
+++ src/config/global.js
@@ -205,7 +205,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/63520002_CF02_DU.pdf',
+        download: 'downloads/63520002_CF02_DU.zip',
       },
       {
         icono: 'fas fa-download',
